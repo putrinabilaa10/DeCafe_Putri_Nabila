4ae5c589aac296fd96aeebeb0fe32533a5cfbf26
purtinabliaa10 <putrinabila1007@gmail.com>
2023-10-30 16:32:54 +0700

    connect

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942_putri","|L$Y?qV6","u837952942_putri");
     if(!$conn){
         echo "Gagal Koneksi";
     }
